c48d59c87b23215b84479fbd47803b9747920fc3
zidanekaito <149668770+zidanekaito@users.noreply.github.com>
2024-06-05 11:51:13 +0700

    App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { AppProvider } from './context/AppContext';
 import CartValue from './components/CartValue';
 import ExpenseList from './components/ExpenseList';
